70cfae6c790e1b5ce6fcc9378319a6c0ef52580e
Oskarovsky <oskar.slyk@gmail.com>
2023-12-03 18:06:19 +0100

    client http;

--- src/main/kotlin/com/server/jobboxapp/entity/Domain.kt
+++ src/main/kotlin/com/server/jobboxapp/entity/Domain.kt
@@ -1,28 +1,29 @@
 package com.server.jobboxapp.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 import java.time.LocalDate
 
 @Entity
 data class Offer(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Long = 0,
 
     val title: String,
     val description: String,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "employer_id")
     val employer: Employer,
 
-////    @Column(nullable = false)
-//    val creationDate: LocalDate = LocalDate.now(),
-//
-////    @Column(nullable = false)
-//    val lastModificationDate: LocalDate = LocalDate.now(),
-//
-////    @Column(nullable = false)
-//    val expirationDate: LocalDate = LocalDate.now().plusDays(30)
+    @Column(nullable = false)
+    val creationDate: LocalDate = LocalDate.now(),
+
+    @Column(nullable = false)
+    val lastModificationDate: LocalDate = LocalDate.now(),
+
+    @Column(nullable = false)
+    val expirationDate: LocalDate = LocalDate.now().plusDays(30)
 
 
 )
@@ -30,12 +31,13 @@ data class Offer(
 @Entity
 data class Employer(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Long = 0,
 
     val name: String,
     val description: String,
 
-    @OneToMany(mappedBy = "employer")
+    @OneToMany(mappedBy = "employer", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @JsonIgnore
     val offers: List<Offer> = mutableListOf()
 )
 
